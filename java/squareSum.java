a79ed6becbb12122e9e9878e5a0305f505bc880e
dstacho1 <33771566+dstacho1@users.noreply.github.com>
2020-09-16 12:46:30 -0500

    Update squareSum.java
    Added description of function.

--- java/squareSum.java
+++ java/squareSum.java
@@ -1,5 +1,7 @@
 
-
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 class squareSum {
   public static void main(String[] args) {
     int[] test = {1,2,2};
